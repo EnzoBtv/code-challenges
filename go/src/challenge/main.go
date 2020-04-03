cd76c2bcd70a380ec667b32090ac34c9aacb3bed
EnzoBtv <enzo_batrov@hotmail.com>
2020-04-03 00:50:28 -0300

    single number and happy number challenges

--- go/src/challenge/main.go
+++ go/src/challenge/main.go
@@ -191,9 +191,81 @@ func wordBreak(input string, tokens []string) bool {
 	return false
 }
 
+// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+// Note:
+
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+// Example 1:
+
+// Input: [2,2,1]
+// Output: 1
+// Example 2:
+
+// Input: [4,1,2,1,2]
+// Output: 4
+
+func singleNumber(nums []int) int {
+	frequency := make(map[int]int)
+
+	for _, num := range nums {
+		frequency[num]++
+	}
+
+	for num, times := range frequency {
+		if times == 1 {
+			return num
+		}
+	}
+
+	return 0
+}
+
+// Write an algorithm to determine if a number is "happy".
+
+// A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+// Example:
+
+// Input: 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+func isHappy(n int) bool {
+	newNumber := 0
+	visited := make(map[int]struct{})
+	for {
+		splitedNumber := strings.Split(strconv.Itoa(n), "")
+
+		for _, num := range splitedNumber {
+			formatedNumber, _ := strconv.Atoi(num)
+
+			newNumber += int(math.Pow(float64(formatedNumber), 2.0))
+		}
+
+		if newNumber == 1 {
+			return true
+		}
+
+		n = newNumber
+
+		if _, ok := visited[n]; ok {
+			return false
+		}
+
+		visited[newNumber] = struct{}{}
+
+		newNumber = 0
+	}
+}
 func main() {
 
 	//matrix := [][]int{{4, 8}, {6, 10}}
 
-	fmt.Println(compareStringWithSlice("abc", []string{"ab", "bc", "a"}))
+	fmt.Println(isHappy(2))
 }
